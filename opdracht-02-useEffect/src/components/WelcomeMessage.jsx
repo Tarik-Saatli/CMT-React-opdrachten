963bf58ae344f1d87b32770c02a051251d3942b7
Tarik-Saatli <9027185@student.zadkine.nl>
2025-10-14 13:42:17 +0200

    Opdracht 3: Welkom

---
+++ opdracht-02-useEffect/src/components/WelcomeMessage.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+const WelcomeMessage = () => {
+    const [message, setMessage] = useState();
+
+    useEffect(() => {
+        let currentTime = new Date()
+        let hours = currentTime.getHours()
+        let newMessage = ""
+
+        if (hours < 12) {
+            newMessage = "Goedemorgen"
+        }
+        else if (hours < 18){
+            newMessage = "goedemiddag"
+        }
+        else {
+    newMessage = "goedenavond"
+}
+
+setMessage(newMessage)
+}, []);
+
+
+
+return ( 
+    <h2>{message}</h2>
+ );
+}
+
+export default WelcomeMessage;
