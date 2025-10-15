d7293f178bf069bb72019d9bb61289fe7b602aa5
Tarik-Saatli <9027185@student.zadkine.nl>
2025-10-15 11:32:53 +0200

    Opdracht 1.3: Lees-teller per boek

--- uitleg-lessen/src/components/Book.jsx
+++ uitleg-lessen/src/components/Book.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 const Book = ({title, author, image}) => {
+    const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0)
+
+function verhoogTeller() {
+setAantalKeerGelezen(aantalKeerGelezen+1)
+}
+
     return ( 
         <section >
             <img className="img" src={image} alt={title} />
             <h2>{title}</h2>
             <p>{author}</p>
+            <button onClick={verhoogTeller}>Keer gelezen:  {aantalKeerGelezen}</button>
         </section>
      );
 }
