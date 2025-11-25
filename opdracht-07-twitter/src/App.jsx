5d3f79b5352f6246e664b9830a4c17b40c0845c1
Tarik-Saatli <9027185@student.zadkine.nl>
2025-11-25 13:05:58 +0100

    update

--- opdracht-07-twitter/src/App.jsx
+++ opdracht-07-twitter/src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css'
+import CreateMessage from './components/CreateMessage'
+import Messagelist from './components/MessageList';
 
 function App() {
+  const [textInput, setTextInput] = useState('');
+  const [message, setMessage] = useState([]);
+
+  let name = "Tarik"
 
   return (
     <>
-      <h1>Opdracht 7 - Twitter</h1>
+      <CreateMessage textInput={textInput} setTextInput={setTextInput} message={message} setMessage={setMessage} />
+      <Messagelist name={name} message={message} />
+
     </>
-  )
+  );
 }
 
 export default App
---
+++ opdracht-07-twitter/src/components/MessageList.jsx
@@ -0,0 +1,17 @@
+import Message from "./Message";
+
+const MessageList = ({ name, message }) => {
+    return (
+        <>
+            {message.map((m) => (
+                <Message name={name} message={m
+                } />
+            ))}
+
+
+
+        </>
+    );
+}
+
+export default MessageList;
