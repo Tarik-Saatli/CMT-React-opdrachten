ea433db214c2fe163c8023a5b3e87a6fdc062afe
Tarik-Saatli <9027185@student.zadkine.nl>
2025-12-03 11:40:47 +0100

    update

--- uitleg-lessen/src/components/BookList.jsx
+++ uitleg-lessen/src/components/BookList.jsx
@@ -1,37 +1,53 @@
 import Book from "./Book";
 import { useState } from "react";
 import BookCounter from "./BookCounter";
+import BookData from "../book-data.js";
 
 const BookList = () => {
 
-  const [books, setbooks] = useState([
-    {
-      title: 'het leven van een loser',
-      author: 'Jason',
-      image: './images/book-1.png',
-    },
-    {
-      title: 'killer instinct',
-      author: 'Jason',
-      image: './images/book-2.png',
-    },
-  ]
-  );
+  const [books, setbooks] = useState(BookData);
 
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    let newSearch = e.target.value
+
+    setSearchInput(newSearch);
+
+    const filteredBooks = BookData.filter((book) =>
+
+      book.title.toLowerCase().includes(newSearch.toLowerCase())
+    );
+
+    setbooks(filteredBooks);
+
+  };
 
 
   return (
     <section className="book-container">
+       <div>
+            <input
+              type="text"
+              placeholder="Zoek een titel op"
+              onChange={handleChange}
+              value={searchInput}
+              name="search"
+            />
+          </div>
       <BookCounter
-      aantal={books.length}/>
+        aantal={books.length} />
 
       {books.map((book) => (
-        <Book
+        <><Book
           title={book.title}
           author={book.author}
-          image={book.image}
+          image={book.image} />
 
-        />
+
+        </>
       ))}
 
 
