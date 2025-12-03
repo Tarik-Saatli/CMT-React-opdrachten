775ddd63c9b837cd74c01b106a0789b6be670e86
Tarik-Saatli <9027185@student.zadkine.nl>
2025-12-03 13:10:40 +0100

    update

--- opdracht-06-zoo-app/src/Pages/HomePages.jsx
+++ opdracht-06-zoo-app/src/Pages/HomePages.jsx
@@ -6,16 +6,43 @@ import Animal from '../components/Animal';
 const Home = () => {
   const navigate = useNavigate();
   const [animalList, setAnimalList] = useState(animals);
+  const [searchInput, setSearchInput] = useState('');
 
   const goToDetail = (animalId) => {
     navigate(`/animal/${animalId}`);
   };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    let newSearch = e.target.value
+
+    setSearchInput(newSearch);
+
+    const filteredAnimal = animals.filter((animal) =>
+
+      animal.name.toLowerCase().includes(newSearch.toLowerCase())
+    );
+
+    setAnimalList(filteredAnimal);
+  }
+
 
   return (
     <section>
       <h1>Welkom bij onze winkel!</h1>
       <h2>Onze Producten</h2>
       
+      <div>
+        <input
+          type="text"
+          placeholder="Zoek een titel op"
+          onChange={handleChange}
+          value={searchInput}
+          name="search"
+        />
+      </div>
+
       {animalList.map(animal => (
         <Animal
           key={animal.id}
