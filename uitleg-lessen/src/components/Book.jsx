348a6a157d542be163857e480f186189813edf5e
Tarik-Saatli <9027185@student.zadkine.nl>
2025-12-16 11:06:09 +0100

    opdracht 13.1

--- uitleg-lessen/src/components/Book.jsx
+++ uitleg-lessen/src/components/Book.jsx
@@ -1,21 +1,35 @@
 import { useState } from "react";
 
-const Book = ({title, author, image, category}) => {
+const Book = ({ title, author, image, category }) => {
     const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0)
 
-function verhoogTeller() {
-setAantalKeerGelezen(aantalKeerGelezen+1)
-}
+    const [liked, setLiked] = useState(false)
+    function toggleLike() {
+        setLiked(!liked)
 
-    return ( 
+    }
+
+    function verhoogTeller() {
+        setAantalKeerGelezen(aantalKeerGelezen + 1)
+    }
+
+    return (
         <section >
             <img className="img" src={image} alt={title} />
             <h2>{title}</h2>
             <p>{author}</p>
             <p>{category}</p>
+
+            <div className="favorite-section">
+                <button onClick={toggleLike}>
+                    {liked ? '❤️' : '🤍'}
+                </button>
+                {liked && <p className="favorited-text">Toegevoegd aan favorieten</p>}
+            </div>
+
             <button onClick={verhoogTeller}>Keer gelezen:  {aantalKeerGelezen}</button>
         </section>
-     );
+    );
 }
- 
+
 export default Book;
